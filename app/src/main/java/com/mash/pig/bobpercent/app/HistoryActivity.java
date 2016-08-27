4ff9515ed24d2bac8d30b6d41107db4d09d295ce
JIMIN\Jimin <windyhavem@gmail.com>
2016-08-28 03:22:06 +0900

    All Activities, xml files edit

---
+++ app/src/main/java/com/mash/pig/bobpercent/app/HistoryActivity.java
@@ -0,0 +1,9 @@
+package com.mash.pig.bobpercent.app;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jimin on 2016-08-27.
+ */
+public class HistoryActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/mash/pig/bobpercent/loginActivity.java
+++
@@ -1,7 +0,0 @@
-package com.mash.pig.bobpercent;
-
-/**
- * Created by Jimin on 2016-08-27.
- */
-public class loginActivity {
-}
